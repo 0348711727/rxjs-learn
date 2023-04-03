6802aef4fd3bb9d91c097235b9a0ed414d7561ab
0348711727 <65222273+0348711727@users.noreply.github.com>
2023-04-03 22:11:17 +0700

    Initial commit

---
+++ index.ts
@@ -0,0 +1,31 @@
+import './style.css';
+
+import { of, map, pipe, Observable, from } from 'rxjs';
+import { ajax, AjaxResponse } from 'rxjs/ajax';
+
+// const ajax$ = ajax('https://random-data-api.com/api/v2/users')
+
+// ajax$.subscribe((value: AjaxResponse<{ [key: string]: string }>) =>
+//   console.log('sub1 ' + value.response.first_name)
+// );
+// ajax$.subscribe((value: AjaxResponse<{ [key: string]: string }>) =>
+//   console.log('sub2 ' + value.response.first_name)
+// );
+// ajax$.subscribe((value: AjaxResponse<{ [key: string]: string }>) =>
+//   console.log('sub3 ' + value.response.first_name)
+// );
+
+from([1, 2, 3, 4]).subscribe({
+  next: (value) => console.log(value),
+  complete: () => console.log('Completed'),
+});
+
+function ownerMap(...args: number[]): Observable<number> {
+  console.log(args);
+  return new Observable<number>((subscriber) => {
+    args.map((value) => {
+      subscriber.next(value);
+    });
+    subscriber.complete();
+  });
+}
