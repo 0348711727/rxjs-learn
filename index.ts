9d85f6285f18628dabc6c58c0614fcdc2a47d0c2
0348711727 <65222273+0348711727@users.noreply.github.com>
2023-04-04 16:50:26 +0700

    time/rxjs

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import './style.css';
 
-import { of, map, pipe, Observable, from } from 'rxjs';
+import { of, map, pipe, Observable, from, timer } from 'rxjs';
 import { ajax, AjaxResponse } from 'rxjs/ajax';
 
 // const ajax$ = ajax('https://random-data-api.com/api/v2/users')
@@ -15,17 +15,27 @@ import { ajax, AjaxResponse } from 'rxjs/ajax';
 //   console.log('sub3 ' + value.response.first_name)
 // );
 
-from([1, 2, 3, 4]).subscribe({
-  next: (value) => console.log(value),
+// from([1, 2, 3, 4]).subscribe({
+//   next: (value) => console.log(value),
+//   complete: () => console.log('Completed'),
+// });
+
+// function ownerMap(...args: number[]): Observable<number> {
+//   console.log(args);
+//   return new Observable<number>((subscriber) => {
+//     args.map((value) => {
+//       subscriber.next(value);
+//     });
+//     subscriber.complete();
+//   });
+// }
+
+const subscription = timer(2000).subscribe({
+  next: () => console.log('active'),
   complete: () => console.log('Completed'),
 });
 
-function ownerMap(...args: number[]): Observable<number> {
-  console.log(args);
-  return new Observable<number>((subscriber) => {
-    args.map((value) => {
-      subscriber.next(value);
-    });
-    subscriber.complete();
-  });
-}
+setTimeout(() => {
+  console.log('unsubscribe');
+  subscription.unsubscribe();
+}, 3000);
